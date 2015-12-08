b20fd67674e9f0c6f9284def2e7eb82345782a40
Adam Bogdał <adam@bogdal.pl>
2015-12-08 01:29:20 +0100

    Refactor classes

--- freepacktbook/__init__.py
+++ freepacktbook/__init__.py
@@ -21,31 +21,31 @@ class FreePacktBook(object):
             'email': self.email,
             'password': self.password,
             'form_id': 'packt_user_login_form'})
-        book_details = self.get_book_details(response)
-        parser = BeautifulSoup(response.text, 'html.parser')
-        claim_url = self.base_url + parser.find('div', {
-            'class': 'free-ebook'}).a['href']
-        response = self.session.get(claim_url)
+        page = BeautifulSoup(response.text, 'html.parser')
+        claim_url = page.find('div', {'class': 'free-ebook'}).a['href']
+        response = self.session.get(self.base_url + claim_url)
         assert response.status_code == 200
-        return book_details
+        return self.get_book_details(page)
 
-    def get_book_details(self, response=None):
-        if response is None:
+    def get_book_details(self, page=None):
+        if page is None:
             response = self.session.get(self.url)
-        parser = BeautifulSoup(response.text, 'html.parser')
-        summary = parser.find('div', {'class': 'dotd-main-book-summary'})
-        main_book_image = parser.find('div', {'class': 'dotd-main-book-image'})
+            page = BeautifulSoup(response.text, 'html.parser')
+        summary = page.find('div', {'class': 'dotd-main-book-summary'})
+        main_book_image = page.find('div', {'class': 'dotd-main-book-image'})
         return {
             'title': summary.find('div', {'class': 'dotd-title'}).getText().strip(),
             'description': summary.find('div', {'class': None}).getText().strip(),
-            'url': self.base_url + main_book_image.a['href'],
+            'url': self.url,
+            'book_url': self.base_url + main_book_image.a['href'],
             'image_url': 'https:%s' % main_book_image.img['src']}
 
 
 def claim_free_ebook():
     client = FreePacktBook(
         environ.get('PACKTPUB_EMAIL'), environ.get('PACKTPUB_PASSWORD'))
+    book = client.claim_free_ebook()
 
     slack_notification = SlackNotification(
-        client, environ.get('SLACK_URL'), environ.get('SLACK_CHANNEL'))
-    slack_notification.claim_free_ebook()
+        environ.get('SLACK_URL'), environ.get('SLACK_CHANNEL'))
+    slack_notification.notify(book)
--- freepacktbook/slack.py
+++ freepacktbook/slack.py
@@ -4,28 +4,26 @@ import requests
 
 class SlackNotification(object):
 
-    def __init__(self, client, slack_url, channel):
-        self.client = client
+    def __init__(self, slack_url, channel):
         self.slack_url = slack_url
         self.channel = channel
 
-    def claim_free_ebook(self):
-        book_details = self.client.claim_free_ebook()
-        if self.slack_url and self.channel:
-            data = {
-                'channel': self.channel,
-                'username': 'PacktPub Free Learning',
-                'icon_emoji': ':books:',
-                'attachments': [{
-                    'fallback': 'Today\'s Free eBook: %s' % book_details['title'],
-                    'pretext': 'Today\'s Free eBook:',
-                    'color': '#ff7f00',
-                    'fields': [{
-                        'title': book_details['title'],
-                        'value': '%s %s' % (
-                            book_details['description'], self.client.url),
-                        'short': False
-                    }]
-                }]}
-            requests.post(self.slack_url, data={'payload': json.dumps(data)})
-        return book_details
+    def notify(self, data):
+        if not all([self.slack_url, self.channel]):
+            return
+        payload = {
+            'channel': self.channel,
+            'username': 'PacktPub Free Learning',
+            'icon_emoji': ':books:',
+            'attachments': [{
+                'fallback': 'Today\'s Free eBook: %s' % data['title'],
+                'pretext': 'Today\'s Free eBook:',
+                'color': '#ff7f00',
+                'fields': [{
+                    'title': data['title'],
+                    'value': '%s %s' % (
+                        data['description'], data['url']),
+                    'short': False
+                }]
+            }]}
+        requests.post(self.slack_url, data={'payload': json.dumps(payload)})
