568a95c07864c526034a4035f36170fa432b39a8
Adam Bogdał <adam@bogdal.pl>
2016-02-11 09:54:47 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='freepacktbook',
-    version='0.0.7',
+    version='0.0.8',
     description='Claim Your Free PacktPub eBook',
     author='Adam Bogdał',
     author_email='adam@bogdal.pl',
