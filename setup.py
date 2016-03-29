50439b3db43c15c1a4bd3963197f4de49ce292a6
Adam Bogdał <adam@bogdal.pl>
2016-03-29 11:01:58 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class PyTest(TestCommand):
 
 setup(
     name='freepacktbook',
-    version='0.0.10',
+    version='0.0.11',
     description='Claim Your Free PacktPub eBook',
     author='Adam Bogdał',
     author_email='adam@bogdal.pl',
